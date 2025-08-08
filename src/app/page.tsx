f65a2eee9aab1308f4f1a905c02a1bdc5470716a
Thuận Lee <66121332+leenauht@users.noreply.github.com>
2025-08-08 11:15:43 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import RoomList from './_components/room/roomList';
 export default function Home() {
   return (
     <div className="text-base">
-      <p className="pt-40 text-red-600">hello</p>
       <RoomList />
     </div>
   );
